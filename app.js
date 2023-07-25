8f5fec4edd9497615624167739067386a461e47c
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2023-07-25 22:30:30 +0200

    Added search functionality

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express')
-
-
-//const path = require('path');
-//sconst __dirname = path.resolve();
+const axios = require('axios');
 
 const app = express();
 
@@ -17,23 +14,30 @@ app.get('/info', function (req, res){
     res.sendFile(__dirname + '/web/info.html')
 
 });
-//codice crack roba
+
 app.get('/search', function (req, res){
-    console.log('hi')
-    res.send ('test')
-
-    const request = require('request')
-    request('https://server23.animesaturnstream.com/DDL/ANIME/', function (
-    error,
-    response,
-     body
-    ){
-    console.error('error:', error)
-    console.log('body:', body)
-    });
-
+   console.log(req.param('query'))
+   const url = 'https://www.animeworld.so/search?keyword=' + req.param('query');
+   console.log(url)
+axios.get(url)
+  .then(response => {
+    const htmlContent = response.data;
+	
+	const anipattern = /<div[^>]*class="inner"[^>]*>[\s\S]*?<\/div>/g;
+	
+    const matches = htmlContent.match(anipattern);
+	
+    matches.forEach(element => {
+		console.log(element);
+	});
+  })
+  .catch(error => {
+    console.error('Error fetching website:', error.message);
+  });
+	res.send("search page")
 });
 
+console.log("Server is running on localhost8080")
 app.listen(8080, function(){
     console.log("Server is running on localhost8080")
 });
