8d04920673320106c5e983a8effb4344ffb6c4ba
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2023-07-26 21:23:00 +0200

    Update search function

--- app.js
+++ app.js
@@ -20,24 +20,53 @@ app.get('/info', function (req, res){
 });
 
 
+app.get('/play', function (req, res){
+    res.sendFile(__dirname + '/web/play.html')
+
+});
+
 const cerca1 = fs.readFileSync("./web/cerca1.html");
 const cerca2 = fs.readFileSync("./web/cerca2.html");
 
 var html = ""
-
 app.get('/search', function (req, res){
    const url = 'https://www.animeworld.so/search?keyword=' + req.param('query');
 axios.get(url)
   .then(response => {
     
-	const dom = new JSDOM(response.data)
+	const dom = new JSDOM(response.data);
 	
-	let matches = dom.window.document.querySelectorAll(".item > .inner > a")
+	let matches = dom.window.document.querySelectorAll(".item > .inner > a");
 	
+	// Weird but works fast
+	
+	var group = false
+	var togroup = ""
     matches.forEach(element => {
-		if (!element.innerHTML.includes("Ricerca nel sito")){
-			html+='<div class="titolo">' + element.outerHTML +"</div>"
-		}
+			fixed = element.outerHTML
+			if (fixed.includes('<div class="dub">DUB</div>')){fixed = fixed.replace('<div class="dub">DUB</div>',"")}
+			
+			if (fixed.includes('<div class="ova">OVA</div>')){fixed = fixed.replace('<div class="ova">OVA</div>',"")}
+			
+			if (fixed.includes('<div class="movie">Movie</div>')){fixed = fixed.replace('<div class="movie">Movie</div>',"")}
+			
+			if (fixed.includes('<div class="special">Special</div>')){fixed = fixed.replace('<div class="special">Special</div>',"")}
+			
+			
+			if (group == false){
+				togroup=fixed;
+				group = true;
+				
+			}
+			else{
+				console.log(togroup + fixed)
+				console.log("-----------------------------------------")
+				html+='<div class="titolo">' + togroup + fixed + "</div>";
+				group = false
+				togroup = ""
+			}
+										
+		
 	});
 	
 	res.send(cerca1 + html + cerca2)
